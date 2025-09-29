3839628ebd2dda62f4989c10406af2432eb591ca
Joel <joeldpv6@icloud.com>
2025-09-28 23:51:34 -0400

    Basic model architecture

--- main.py
+++ main.py
@@ -39,14 +39,83 @@ test_data = datasets.ImageFolder(root=TEST_PATH,
                          target_transform=None)
 
 BATCH_SIZE = 32
+NUM_WORKERS = os.cpu_count()
 
 train_dataloader = DataLoader(dataset=train_data, 
                               batch_size=BATCH_SIZE,
-                              shuffle=True)
+                              shuffle=True,
+                              num_workers=NUM_WORKERS)
 
 test_dataloader = DataLoader(dataset=test_data, 
                               batch_size=BATCH_SIZE,
-                              shuffle=True)
+                              shuffle=True,
+                              num_workers=NUM_WORKERS)
 
-print(train_data[0][0].shape)
-print(train_data[0][1])
+class AnimalNN(nn.Module):
+    def __init__(self, 
+                 input_shape: int,
+                 hidden_units: int,
+                 output_shape: int) -> None:
+        super().__init__()
+
+        self.conv_block_1 = nn.Sequential(
+            nn.Conv2d(in_channels=input_shape,
+                      out_channels=hidden_units,
+                      kernel_size=3,
+                      stride=1,
+                      padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=hidden_units,
+                      out_channels=hidden_units,
+                      kernel_size=3,
+                      stride=1,
+                      padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2,
+                         stride=2))
+        
+        self.conv_block_2 = nn.Sequential(
+            nn.Conv2d(in_channels=hidden_units,
+                      out_channels=hidden_units,
+                      kernel_size=3,
+                      stride=1,
+                      padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=hidden_units,
+                      out_channels=hidden_units,
+                      kernel_size=3,
+                      stride=1,
+                      padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2,
+                         stride=2))
+
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(in_features=hidden_units,
+                      out_features=output_shape)
+        )
+
+    def forward(self, x: int):
+        x = self.conv_block_1(x)
+        print(x.shape)
+        x = self.conv_block_2(x)
+        print(x.shape)
+        print(torch.flatten(x).shape)
+        x = self.classifier(x)
+        print(x.shape)
+
+        return x
+        # return self.classifier(self.conv_block_2(self.conv_block_1(x)))
+
+print(train_data[0][0])
+
+image_batch, label_batch = next(iter(train_dataloader))
+print(image_batch.shape)
+print(label_batch.shape)
+
+model = AnimalNN(input_shape=3,
+                 hidden_units=10,
+                 output_shape=3).to(device)
+
+model(image_batch.to(device))
